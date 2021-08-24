ee7bae6788e270b01d8e2ea8fd0cc261f3d4d6be
LeandroGonçalves <lezinhomococa@hotmail.com>
2021-08-24 19:58:15 -0300

    add progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,17 @@ import '../styles/global.css'
 import React from "react";
 import { ToastContainer } from 'react-toastify';
 import { AuthProvider } from '../context/Authcontext';
+import NextNProgress from 'nextjs-progressbar'
 function MyApp({ Component, pageProps, router }) {
   return (
     <AuthProvider>
       <ToastContainer/>
+      <NextNProgress
+        color="#67b39e"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+      />
       <Component {...pageProps}  key={router.route}/>
     </AuthProvider>
   )
